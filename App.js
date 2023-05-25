e18cca90558e416f35e313fa766d5b11f00f39bf
Amil Silahic <jinobie02@gmail.com>
2023-05-25 14:36:19 -0600

    fetching data from TMDB API

--- App.js
+++ App.js
@@ -1,13 +1,39 @@
 import { StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
 
 import { StatusBar } from "expo-status-bar";
 
 export default function App() {
+  const [moviesData, setMoviesData] = useState([]);
+  const url =
+    "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1";
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: process.env.TMDB_AUTH,
+    },
+  };
+  useEffect(() => {
+    const getData = async () => {
+      const data = await fetch(url, options);
+
+      const fullData = await data.json();
+
+      setMoviesData(fullData.results);
+    };
+    getData();
+  }, []);
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-
       <Text>Hello World</Text>
+
+      {moviesData.map((data) => {
+        return <Text key={data.id}>{data.original_title}</Text>;
+      })}
     </View>
   );
 }
