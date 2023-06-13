c84d615e116df5250765e147a03fb07e715702e6
Amil Silahic <jinobie02@gmail.com>
2023-06-13 15:28:35 -0600

    adding poster image for the movie

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 import { StatusBar } from "expo-status-bar";
 import useFetchData from "./hooks/useFetchData";
@@ -6,12 +6,25 @@ import useFetchData from "./hooks/useFetchData";
 export default function App() {
   const { moviesData } = useFetchData();
 
+  console.log(moviesData[0]);
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
 
       {moviesData.map((data) => {
-        return <Text key={data.id}>{data.original_title}</Text>;
+        return (
+          <>
+            <Text key={data.id}>{data.original_title}</Text>
+
+            <Image
+              style={styles.image}
+              source={{
+                uri: `https://image.tmdb.org/t/p/original${data.poster_path}`,
+              }}
+            />
+          </>
+        );
       })}
     </View>
   );
@@ -23,5 +36,10 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: "contain",
   },
 });
