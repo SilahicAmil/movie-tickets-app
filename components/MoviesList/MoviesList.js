9ab895c81abb702b65ba08795f0f63b530be1e8f
Amil Silahic <jinobie02@gmail.com>
2023-06-15 16:08:05 -0600

    adding movies list component

---
+++ components/MoviesList/MoviesList.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const MoviesList = () => {
+  return (
+    <>
+      <View>
+        <Text>Movies List FlatList goes here</Text>
+      </View>
+    </>
+  );
+};
+
+export default MoviesList;
