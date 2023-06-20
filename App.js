72aa66df1c1bd6e9bbad2dabebd8685a06a86023
Amil Silahic <jinobie02@gmail.com>
2023-06-20 11:55:35 -0600

    adding rneui and creating movies list and passing props

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 
 import MoviesList from "./components/MoviesList/MoviesList";
 import { StatusBar } from "expo-status-bar";
@@ -6,31 +6,28 @@ import useFetchData from "./hooks/useFetchData";
 
 export default function App() {
   const { moviesData } = useFetchData();
+  console.log(moviesData[0]);
 
   const renderMoviesListItems = (itemData) => {
     // navigate on press
 
     return (
       <MoviesList
-        imagePath={itemData.item.imagePath}
+        imagePath={itemData.item.poster_path}
         title={itemData.item.original_title}
       />
     );
   };
 
   return (
-    <View style={styles.container}>
+    <View>
       <StatusBar style="auto" />
-      {/*  FlatList here   */}
+      <FlatList
+        data={moviesData}
+        renderItem={renderMoviesListItems}
+        keyExtractor={(item) => item.id}
+        numColumns={2}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- components/MoviesList/MoviesList.js
+++ components/MoviesList/MoviesList.js
@@ -1,20 +1,43 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
+
+import { Card } from "@rneui/themed";
 
 const MoviesList = ({ imagePath, title }) => {
+  console.log(imagePath);
   return (
     <>
-      <View>
-        {/* put imagePath in URI */}
-        <Image />
-        <Text>{title}</Text>
-      </View>
+      {/* add onPress */}
+      <SafeAreaView style={styles.container}>
+        <Card containerStyle={styles.card}>
+          <Image
+            source={{ uri: `https://image.tmdb.org/t/p/original${imagePath}` }}
+            style={styles.image}
+          />
+          <Card.Title style={styles.text}>{title}</Card.Title>
+        </Card>
+      </SafeAreaView>
     </>
   );
 };
 
 const styles = StyleSheet.create({
   // add styling
-  container: {},
+  container: {
+    flex: 1,
+  },
+  card: {
+    height: 420,
+    borderWidth: 0,
+  },
+  image: {
+    width: "100%",
+    height: 300,
+    resizeMode: "contain",
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 24,
+  },
 });
 
 export default MoviesList;
