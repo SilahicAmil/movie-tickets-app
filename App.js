2b9bc22097c391dabc296f1b50ddfc41579939d5
Amil Silahic <jinobie02@gmail.com>
2023-06-21 11:56:32 -0600

    feat: :zap: Creating Tab Navigator
    Created tab navigator for different tabs. Eventually will be like Home, Account, Cart and etc

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import HomeScreen from "./screens/HomeScreen";
 import MoviesDetailsScreen from "./screens/MoviesDetailsScreen";
 import { NavigationContainer } from "@react-navigation/native";
+import TabNavigation from "./navigation/TabNavigation";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator initialRouteName="TabHome">
+        <Stack.Screen
+          name="Home"
+          component={TabNavigation}
+          options={{
+            title: "Omniplex",
+          }}
+        />
         <Stack.Screen
           name="MoviesDetailsScreen"
           component={MoviesDetailsScreen}
--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,3 +1,34 @@
-const TabNavigation = () => {};
+import HomeScreen from "../screens/HomeScreen";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const Tab = createBottomTabNavigator();
+
+const TabNavigation = () => {
+  return (
+    <Tab.Navigator initialRouteName="TabHome">
+      <Tab.Screen
+        name="TabHome"
+        component={HomeScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Cart"
+        component={HomeScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Cart2"
+        component={HomeScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
 export default TabNavigation;
