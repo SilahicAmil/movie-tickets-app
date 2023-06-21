1bbe35434d0f03120c3550d4e3cb114a1411330a
Amil Silahic <jinobie02@gmail.com>
2023-06-21 11:47:44 -0600

    feat: :zap: Creating Basic Details Screen
    Creating basic details screen, just getting the layout for now. Will abstract components later and do final styling

--- screens/MoviesDetailsScreen.js
+++ screens/MoviesDetailsScreen.js
@@ -1,11 +1,43 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
-const MoviesDetailsScreen = ({}) => {
+import { Card } from "@rneui/themed";
+
+const MoviesDetailsScreen = ({ route }) => {
+  const { original_title, vote_average, release_date, overview, poster_path } =
+    route.params;
+
   return (
-    <View>
-      <Text>123</Text>
+    <View style={styles.container}>
+      <Card containerStyle={styles.card}>
+        <Card.Image
+          source={{
+            uri: `https://image.tmdb.org/t/p/original${poster_path}`,
+          }}
+          style={styles.image}
+        />
+      </Card>
+      <Text>{overview}</Text>
     </View>
   );
 };
 
 export default MoviesDetailsScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    height: "100%",
+    marginBottom: 20,
+  },
+  card: {
+    backgroundColor: "black",
+    width: "100%",
+    margin: 0,
+  },
+  image: {
+    height: 300,
+    width: "100%",
+    resizeMode: "stretch",
+  },
+});
