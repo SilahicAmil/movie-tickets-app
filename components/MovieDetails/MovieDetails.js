d0cec3f8aca065bc95ff300f504ef87aada4679b
Amil Silahic <jinobie02@gmail.com>
2023-06-22 16:51:03 -0600

    creating skeleton movie details component

---
+++ components/MovieDetails/MovieDetails.js
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+
+const MovieDetails = ({ title, description, rating, release }) => {
+  return (
+    <>
+      <View>{/* put Card componen stuff here and pass props  */}</View>;
+    </>
+  );
+};
+
+export default MovieDetails;
