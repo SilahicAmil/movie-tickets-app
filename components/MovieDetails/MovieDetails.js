c5578a5e56721caff2f620fc756119427e3883d5
Amil Silahic <jinobie02@gmail.com>
2023-06-24 16:15:47 -0600

    feat: :zap: Creating MovieDetails component
    Creating component for movie details and passing props

--- components/MovieDetails/MovieDetails.js
+++ components/MovieDetails/MovieDetails.js
@@ -1,11 +1,55 @@
-import { View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
-const MovieDetails = ({ title, description, rating, release }) => {
+import { Card } from "@rneui/themed";
+
+const MovieDetails = ({ title, description, rating, release, poster_path }) => {
   return (
-    <>
-      <View>{/* put Card componen stuff here and pass props  */}</View>;
-    </>
+    <View style={styles.container}>
+      <Card containerStyle={styles.card}>
+        <Card.Image
+          source={{
+            uri: `https://image.tmdb.org/t/p/original${poster_path}`,
+          }}
+          style={styles.image}
+        />
+      </Card>
+      <View>
+        <Text>{title}</Text>
+        <Text>{rating}</Text>
+        <Text>{release}</Text>
+      </View>
+      <Text style={styles.text}>{description}</Text>
+    </View>
   );
 };
 
 export default MovieDetails;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    height: "100%",
+    marginBottom: 20,
+  },
+  card: {
+    backgroundColor: "black",
+    width: "100%",
+    margin: 0,
+    padding: 10,
+  },
+  image: {
+    height: 300,
+    width: "100%",
+    resizeMode: "stretch",
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: "normal",
+    letterSpacing: 0.7,
+    height: "50%",
+    backgroundColor: "red",
+    margin: 10,
+    padding: 15,
+  },
+});
--- screens/MoviesDetailsScreen.js
+++ screens/MoviesDetailsScreen.js
@@ -1,6 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
-
-import { Card } from "@rneui/themed";
+import MovieDetails from "../components/MovieDetails/MovieDetails";
 
 const MoviesDetailsScreen = ({ route, navigation }) => {
   const { original_title, vote_average, release_date, overview, poster_path } =
@@ -8,49 +6,14 @@ const MoviesDetailsScreen = ({ route, navigation }) => {
 
   // set nav option title to original_title
   return (
-    <View style={styles.container}>
-      {/* create this into its own component  */}
-      <Card containerStyle={styles.card}>
-        <Card.Image
-          source={{
-            uri: `https://image.tmdb.org/t/p/original${poster_path}`,
-          }}
-          style={styles.image}
-        />
-      </Card>
-      <Text style={styles.text}>{overview}</Text>
-    </View>
+    <MovieDetails
+      title={original_title}
+      description={overview}
+      poster_path={poster_path}
+      rating={vote_average}
+      release={release_date}
+    />
   );
 };
 
 export default MoviesDetailsScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "white",
-    height: "100%",
-    marginBottom: 20,
-  },
-  card: {
-    backgroundColor: "black",
-    width: "100%",
-    margin: 0,
-    padding: 10,
-  },
-  image: {
-    height: 300,
-
-    width: "100%",
-    resizeMode: "stretch",
-  },
-  text: {
-    fontSize: 16,
-    fontWeight: "normal",
-    letterSpacing: 0.7,
-    height: "50%",
-    backgroundColor: "red",
-    margin: 10,
-    padding: 15,
-  },
-});
