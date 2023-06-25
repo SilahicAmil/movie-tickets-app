4916734bb6e0608d57e744f19efb564b8bb6d436
Amil Silahic <jinobie02@gmail.com>
2023-06-24 21:46:36 -0600

    creating account screen skeleton

---
+++ screens/AccountScreen.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const AccountScreen = ({}) => {
+  return (
+    <View>
+      <Text>Account Screen</Text>
+    </View>
+  );
+};
+
+export default AccountScreen;
